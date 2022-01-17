e72c912d00129b623086db2f9b899ca385042fbb
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-17 11:02:38 +0300

    Try a solution

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
+const express = require('express');
+
+const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
 
 // let Person;
 
